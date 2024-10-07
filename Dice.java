15ad57bf0bdc1e640dfcce05d427f1b719f271d6
Synth Juice <47258059+SynthJuice@users.noreply.github.com>
2024-10-07 05:18:59 +0200

    Assignment code created during group session uploaded

---
+++ Dice.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dice {
+	
+	private class Player { // All the visibility in this class can be private including the constructor, as it resides within the scope of the Dice-class
+		private int sum;
+		private int dice1, dice2;
+		private int lastDice1, lastDice2;
+		private Random random;
+		
+		private Player(Random random) {
+			this.sum = 0;
+			this.random = random;
+		}
+		
+		private int getSum() {
+			return this.sum;
+		}
+		
+		private void reset() {
+			this.sum = 0;
+		}
+		
+		private void roll() {
+			this.lastDice1 = dice1;
+			this.lastDice2 = dice2;
+			this.dice1 = random.nextInt(1, 6);
+			this.dice2 = random.nextInt(1, 6);
+			sum += dice1 + dice2;
+		}
+	}
+	
+	private Player player1;
+	private Player player2;
+	
+	private boolean playingPlayer = false; // false = player1
+
+	private Random random;
+	
+	private Dice() { // Private is fine here, considering the main method is within this class
+		random = new Random(System.nanoTime()); // Create random number generator using the current time in nanoseconds as a seed
+		player1 = new Player(random);
+		player2 = new Player(random);
+		
+		Scanner scanner = new Scanner(System.in);
+		scanner.useLocale(java.util.Locale.ENGLISH);
+		
+		System.out.println("Hello and welcome to Dice!");
+		System.out.println("Type any string to continue/roll, type \"exit\" to exit game");
+		
+		String input;
+		while (scanner.hasNext()) {
+			input = scanner.nextLine(); // Getting an actual input is necessary as we cannot retrieve keystrokes directly from the OS without using third party libraries
+			if (input.equalsIgnoreCase("exit")) {
+				System.out.println("Goodbye :(");
+				break; // Break loop and to close scanner and exit
+			}
+			Player current = playingPlayer ? player2 : player1;
+			current.roll();
+			
+			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled : " + (current.dice1 + current.dice2) + " | sum : " + current.getSum());
+			playingPlayer = !playingPlayer;
+			
+			if (current.sum >= 40) {
+				System.out.println((playingPlayer ? "Player 2" : "Player 1") + " won");
+				player1.reset();
+				player2.reset();
+				playingPlayer = false;
+				break;
+			}
+
+            // Extra assignemnts goes here
+			
+			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " | Type any string to roll");
+		}
+		
+		scanner.close();
+	}
+	
+	public static void main(String[] args) {
+		new Dice();
+	}
+
+}
