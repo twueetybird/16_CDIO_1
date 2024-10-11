89fe27f84a4a215db39f1672cda0676260ccb88b
Synth Juice <47258059+SynthJuice@users.noreply.github.com>
2024-10-11 15:51:30 +0200

    File fix

--- Dice.java
+++ Dice.java
@@ -4,29 +4,19 @@ import java.util.Scanner;
 public class Dice {
 
 	private class Player { // All the visibility in this class can be private including the constructor, as it resides within the scope of the Dice-class
-		private int sum;
+		private int sum = 0;
 		private int dice1, dice2;
 		private int lastDice1, lastDice2;
-		private Random random;
 
-		private Player(Random random) {
-			this.sum = 0;
-			this.random = random;
+		private void roll(Random random) {
+			this.lastDice1 = this.dice1;
+			this.lastDice2 = this.dice2;
+			this.dice1 = random.nextInt(1, 6 + 1);
+			this.dice2 = random.nextInt(1, 6 + 1);
+			this.sum += this.dice1 + this.dice2;
 		}
 
-		private void reset() {
-			this.sum = 0;
-		}
-
-		private void roll() {
-			this.lastDice1 = dice1;
-			this.lastDice2 = dice2;
-			this.dice1 = random.nextInt(1, 6);
-			this.dice2 = random.nextInt(1, 6);
-			sum += dice1 + dice2;
-		}
-
-		// method to check if roll is 2 identicals and not when both dice are 1
+		// Method to check if roll is 2 identicals and not when both dice are 1
 		private boolean identicalRoll() {
 			return dice1 == dice2 && !(dice1 == 1 && dice2 == 1);
 		}
@@ -39,13 +29,13 @@ public class Dice {
 
 	private Dice() { // Private is fine here, considering the main method is within this class
 		Random random = new Random(System.nanoTime()); // Create random number generator using the current time in nanoseconds as a seed
-		player1 = new Player(random);
-		player2 = new Player(random);
+		player1 = new Player();
+		player2 = new Player();
 
-        runGame();
+        runGame(random);
 	}
 
-    private void runGame() {
+    private void runGame(Random random) {
 		Scanner scanner = new Scanner(System.in);
 		scanner.useLocale(java.util.Locale.ENGLISH);
 
@@ -55,40 +45,53 @@ public class Dice {
 		String input;
 		while (scanner.hasNext()) {
 			input = scanner.nextLine(); // Getting an actual input is necessary as we cannot retrieve keystrokes directly from the OS without using third party libraries
+			long start = System.nanoTime();
 			if (input.equalsIgnoreCase("exit")) {
 				System.out.println("Goodbye :(");
 				break; // Break loop and to close scanner and exit
 			}
 			Player current = playingPlayer ? player2 : player1;
-			current.roll();
+			
+			// Player roll
+			current.roll(random);
 
-			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled: " + current.dice1 + " and "
-					+ current.dice2 + " | sum: " + current.sum);
+			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled: " + current.dice1 + " and " + current.dice2 + " | sum: " + current.sum);
 
-			// Checks if player rolled 2 identical, if not other player gets a turn, otherwise, current player gets another turn
-			if (!current.identicalRoll()) {
-				playingPlayer = !playingPlayer;
-			} else {
-				System.out.println(
-						(playingPlayer ? "Player 2" : "Player 1") + " rolled 2 identical and gets another turn");
+			if (current.sum >= 40 && current.dice1 != current.dice2) {
+				System.out.println((playingPlayer ? "Player 2" : "Player 1") + " has 40 points or more. If they roll two identical dice that are not ones they wil win!");
 			}
 
-			if (current.sum >= 40) {
+			// Checks if player rolled 2 identical, if not other player gets a turn, otherwise, current player gets another turn
+			if (!twoEquals(current, playingPlayer)) {
+				playingPlayer = !playingPlayer; // Setup playingPlayer for player change next turn
+			} else {
+				if (current.sum < 40) {
+					System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled 2 identical and gets another turn");
+				}
+				if (current.dice1 == 6 && current.dice2 == 6) {
+					System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled two sixes! If they roll two sixes next turn again they will win!");
+				}
+			}
+
+			if (current.sum >= 40 + current.dice1 + current.dice2) {
 				if (twoEquals(current, playingPlayer)) {
-					if (ifBothOne(current, playingPlayer)) {
-						break;
+					if (current.identicalRoll()) { // Are they identical but not ones
+						System.out.println((playingPlayer ? "Player 2" : "Player 1") + " won");
+						break; // Stop the game
+					} else if (ifBothOne(current, playingPlayer)) {
+						// The player loses all their points if two 1's are rolled
+						current.sum = 0;
+						System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled two ones and lost all their points | sum: " + current.sum);
 					}
-					System.out.println((playingPlayer ? "Player 2" : "Player 1") + " won");
-					break;
 				}
 			} else {
-				ifBothOne(current, playingPlayer);
 
 				if (ifBothSixes(current, playingPlayer)) {
 					break;
 				}
 			}
-
+			long time = System.nanoTime() - start;
+			System.out.println("Time for one round : " + time + " nanoseconds");
 			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " | Type any string to roll");
 		}
 
@@ -97,9 +100,6 @@ public class Dice {
 
 	private boolean twoEquals(Player current, boolean playingPlayer) {
 		if (current.dice1 == current.dice2) {
-			if (current.dice1 == 1) { // In case both the dice are one
-				return false;
-			}
 			return true;
 		}
 
@@ -107,10 +107,7 @@ public class Dice {
 	}
 
 	private boolean ifBothOne(Player current, boolean playingPlayer) {
-		// The player loses all their points if two 1's are rolled
 		if (current.dice1 == 1 && current.dice2 == 1) {
-			current.reset();
-			System.out.println((playingPlayer ? "Player 2" : "Player 1") + " rolled two ones | sum: " + current.sum);
 			return true;
 		}
 		return false;
